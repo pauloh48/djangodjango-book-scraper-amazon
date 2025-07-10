f9a776a8862c40e19d63486a0c00b4379989de64
phslima <paulohsl48@gmail.com>
2025-07-10 18:07:17 -0300

    fix: path style.css

--- django_selenium/settings.py
+++ django_selenium/settings.py
@@ -117,12 +117,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # arquivos estaticos
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')        # arquivos do usuario
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
